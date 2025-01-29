40e33da9274bf030633c06fbdc7d75659b7e8568
Baibhav Tripathi <34443574+baibhavtripathi@users.noreply.github.com>
2025-01-29 23:45:35 +0530

    Add HighestOccurringElementInArray.java to find the smallest number with the highest frequency in an array

---
+++ Random/HighestOccurringElementInArray.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class HighestOccurringElementInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array Size: ");
+        int arraySize = sc.nextInt();
+
+        int[] nums = new int[arraySize];
+        System.out.println("Enter array elements:");
+        for (int j = 0; j < arraySize; j++) {
+            nums[j] = sc.nextInt();
+        }
+        sc.close();
+
+        System.out.println("Smallest Number With Highest Frequency - "+ mostFrequentElement(nums));
+    }
+
+    public static int mostFrequentElement(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int highestFrequency = 0, smallestNumberWithHighestFrequency = Integer.MAX_VALUE;
+
+        // Build frequency map
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // Find the element with the highest frequency and smallest value
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int key = entry.getKey();
+            int count = entry.getValue();
+            if (count > highestFrequency || (count == highestFrequency && key < smallestNumberWithHighestFrequency)) {
+                highestFrequency = count;
+                smallestNumberWithHighestFrequency = key;
+            }
+        }
+        return smallestNumberWithHighestFrequency;
+    }
+}
